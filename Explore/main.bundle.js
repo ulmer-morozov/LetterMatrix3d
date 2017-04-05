3f3ba29dd0aa311bfc1976f15b43f5501de5a5bb
ulmer-morozov <ulmer.morozov@gmail.com>
2017-04-05 18:25:58 +0300

    ++

--- Explore/main.bundle.js
+++ Explore/main.bundle.js
@@ -86,6 +86,7 @@ var svgMesh3d = __webpack_require__(542);
 var Complex = __webpack_require__(545)(__WEBPACK_IMPORTED_MODULE_1_three__);
 
 var AppComponent = (function () {
+    // private availableLetters = "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     function AppComponent() {
         var _this = this;
         this.mouseSpeed = new __WEBPACK_IMPORTED_MODULE_1_three__["Vector2"]();
@@ -93,6 +94,7 @@ var AppComponent = (function () {
         this.touchSpeed = new __WEBPACK_IMPORTED_MODULE_1_three__["Vector2"]();
         this.normalMaterial = new __WEBPACK_IMPORTED_MODULE_1_three__["MeshNormalMaterial"]();
         this.geometries = [];
+        this.italicGeometries = [];
         this.colliders = [];
         // colors
         this.frontColor = 0xffffff;
@@ -103,7 +105,8 @@ var AppComponent = (function () {
         this.cubeHeight = 1.04;
         this.cubeThick = 0.1;
         this.gap = 0.05;
-        this.fontGeomScale = this.cubeWidth / 4.4;
+        this.fontGeomScale = 0.03;
+        this.availableLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
         this.init = function () {
             var showHelpers = false;
             var width = window.innerWidth;
@@ -159,7 +162,6 @@ var AppComponent = (function () {
             // var light = new THREE.DirectionalLight(0xffffff, 1);
             // light.position.set(1, 1, 1).normalize();
             // this.scene.add(light);
-            var availableLetters = ["G", "j"];
             var loadedCount = 0;
             var currentPos = 0;
             var loadComplete = function () {
@@ -179,29 +181,46 @@ var AppComponent = (function () {
                 document.addEventListener("touchmove", _this.onTouchMove, false);
                 _this.render();
             };
-            var loadSymbol = function () {
-                var letter = availableLetters[currentPos];
-                var glyphPath = "assets/images/letter_" + letter + ".svg";
+            var loadSymbol = function (isItalic) {
+                var letter = _this.availableLetters[currentPos];
+                var letterCode = letter.charCodeAt(0);
+                // debugger;
+                var italicText = isItalic ? "_italic" : "";
+                var glyphName = "letter_" + letterCode + italicText + "_converted.svg";
+                var glyphPath = "assets/images/" + glyphName;
                 loadSvg(glyphPath, function (err, svg) {
                     if (err)
                         throw err;
                     var svgPath = parsePath(svg);
                     var rawGeometry = svgMesh3d(svgPath, {
                         // delaunay: false,
-                        scale: 1,
-                        simplify: 0.01
+                        scale: 10,
+                        simplify: 0.01,
+                        normalize: false
                     });
+                    // let rawGeometryJson = JSON.stringify(rawGeometry);
                     // debugger;
+                    // this.download(`letter_${letterCode}_converted.json`,rawGeometryJson );
                     var complexGeometry = Complex(rawGeometry);
-                    _this.geometries[currentPos] = complexGeometry;
-                    currentPos++;
-                    if (currentPos < availableLetters.length)
-                        loadSymbol();
-                    else
-                        loadComplete();
+                    if (isItalic) {
+                        _this.italicGeometries[currentPos] = complexGeometry;
+                    }
+                    else {
+                        _this.geometries[currentPos] = complexGeometry;
+                    }
+                    if (isItalic == false) {
+                        loadSymbol(true);
+                    }
+                    else {
+                        currentPos++;
+                        if (currentPos < _this.availableLetters.length)
+                            loadSymbol(false);
+                        else
+                            loadComplete();
+                    }
                 });
             };
-            loadSymbol();
+            loadSymbol(false);
             // }
             // ];
             // loadSvg('assets/images/letter_G.svg', (err, svg): void => {
@@ -231,14 +250,25 @@ var AppComponent = (function () {
         //
         //   this.render();
         // }
+        // getRandomLetterGeometry = (): THREE.Geometry => {
+        //   let position = Math.round(Math.random() * this.geometries.length);
+        //   let geometry = ;
+        //   return geometry;
+        // }
         this.createPlateMesh = function () {
-            var frontLetterGeometry = _this.geometries[0];
-            var backLetterGeometry = _this.geometries[1];
+            // position = position % this.geometries.length;
+            var position = Math.round(Math.random() * (_this.geometries.length - 1));
+            var frontLetterGeometry = _this.geometries[position];
+            var backLetterGeometry = _this.italicGeometries[position];
             var frontLetterMesh = new __WEBPACK_IMPORTED_MODULE_1_three__["Mesh"](frontLetterGeometry, _this.frontLetterMaterial);
             var backLetterMesh = new __WEBPACK_IMPORTED_MODULE_1_three__["Mesh"](backLetterGeometry, _this.backLetterMaterial);
             frontLetterMesh.scale.set(_this.fontGeomScale, _this.fontGeomScale, 1);
+            frontLetterMesh.position.x = -0.3;
+            frontLetterMesh.position.y = 0.4;
             frontLetterMesh.position.z = _this.cubeThick + _this.cubeThick / 100;
             backLetterMesh.scale.set(_this.fontGeomScale, _this.fontGeomScale, 1);
+            backLetterMesh.position.x = 0.3;
+            backLetterMesh.position.y = 0.4;
             backLetterMesh.position.z = -_this.cubeThick - _this.cubeThick / 100;
             backLetterMesh.rotation.y = Math.PI;
             // complexMesh.position.z = 50;
@@ -408,6 +438,19 @@ var AppComponent = (function () {
             (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
                 check = true; })(navigator.userAgent || navigator.vendor || window.opera);
             return check;
+        };
+        this.download = function (filename, text) {
+            var pom = document.createElement('a');
+            pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            pom.setAttribute('download', filename);
+            if (document.createEvent) {
+                var event = document.createEvent('MouseEvents');
+                event.initEvent('click', true, true);
+                pom.dispatchEvent(event);
+            }
+            else {
+                pom.click();
+            }
         };
     }
     AppComponent.prototype.ngAfterViewInit = function () {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,6 +46,7 @@ export class AppComponent {
   private normalMaterial = new THREE.MeshNormalMaterial();
 
   private geometries: THREE.Geometry[] = [];
+  private italicGeometries: THREE.Geometry[] = [];
 
   private colliders: THREE.Mesh[] = [];
 
@@ -59,13 +60,16 @@ export class AppComponent {
   private cubeHeight = 1.04;
   private cubeThick = 0.1;
   private gap = 0.05;
-  private fontGeomScale = this.cubeWidth / 4.4;
+  private fontGeomScale = 0.03;
 
   // shared objects
   private frontLetterMaterial: THREE.Material;
   private backLetterMaterial: THREE.Material;
   private plateMaterial: THREE.Material;
   private plateCubeGeometry: THREE.Geometry;
+
+  private availableLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+  // private availableLetters = "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
   constructor() {
 
@@ -158,10 +162,8 @@ export class AppComponent {
     // light.position.set(1, 1, 1).normalize();
     // this.scene.add(light);
 
-    let availableLetters = ["G", "j"];
     let loadedCount = 0;
     let currentPos = 0;
-
 
     let loadComplete = () => {
       // debugger;
@@ -188,9 +190,15 @@ export class AppComponent {
       this.render();
     };
 
-    let loadSymbol = () => {
-      let letter = availableLetters[currentPos];
-      let glyphPath = `assets/images/letter_${letter}.svg`;
+    let loadSymbol = (isItalic: boolean) => {
+      let letter = this.availableLetters[currentPos];
+
+      let letterCode = letter.charCodeAt(0);
+      // debugger;
+
+      let italicText = isItalic ? "_italic" : "";
+      let glyphName = `letter_${letterCode}${italicText}_converted.svg`;
+      let glyphPath = `assets/images/${glyphName}`;
 
       loadSvg(glyphPath, (err, svg): void => {
         if (err)
@@ -199,23 +207,40 @@ export class AppComponent {
         let svgPath = parsePath(svg);
         let rawGeometry = svgMesh3d(svgPath, {
           // delaunay: false,
-          scale: 1,
-          simplify: 0.01
+          scale: 10,
+          simplify: 0.01,
+          normalize: false
         });
+
+        // let rawGeometryJson = JSON.stringify(rawGeometry);
         // debugger;
+        // this.download(`letter_${letterCode}_converted.json`,rawGeometryJson );
+
 
         let complexGeometry: THREE.Geometry = Complex(rawGeometry);
-        this.geometries[currentPos] = complexGeometry;
 
-        currentPos++;
-        if (currentPos < availableLetters.length)
-          loadSymbol();
-        else
-          loadComplete();
+        if (isItalic) {
+          this.italicGeometries[currentPos] = complexGeometry;
+        } else {
+          this.geometries[currentPos] = complexGeometry;
+        }
+
+
+        if (isItalic == false) {
+          loadSymbol(true);
+        } else {
+          currentPos++;
+          if (currentPos < this.availableLetters.length)
+            loadSymbol(false);
+          else
+            loadComplete();
+        }
+
+
       });
     };
 
-    loadSymbol();
+    loadSymbol(false);
     // }
 
     // ];
@@ -249,18 +274,30 @@ export class AppComponent {
   //   this.render();
   // }
 
-  createPlateMesh = (): Plate => {
+  // getRandomLetterGeometry = (): THREE.Geometry => {
+  //   let position = Math.round(Math.random() * this.geometries.length);
+  //   let geometry = ;
+  //   return geometry;
+  // }
 
-    let frontLetterGeometry = this.geometries[0];
-    let backLetterGeometry = this.geometries[1];
+  createPlateMesh = (): Plate => {
+    // position = position % this.geometries.length;
+    let position = Math.round(Math.random() * (this.geometries.length - 1));
+
+    let frontLetterGeometry = this.geometries[position];
+    let backLetterGeometry = this.italicGeometries[position];
 
     let frontLetterMesh = new THREE.Mesh(frontLetterGeometry, this.frontLetterMaterial);
     let backLetterMesh = new THREE.Mesh(backLetterGeometry, this.backLetterMaterial);
 
     frontLetterMesh.scale.set(this.fontGeomScale, this.fontGeomScale, 1);
+    frontLetterMesh.position.x = -0.3;
+    frontLetterMesh.position.y = 0.4;
     frontLetterMesh.position.z = this.cubeThick + this.cubeThick / 100;
 
     backLetterMesh.scale.set(this.fontGeomScale, this.fontGeomScale, 1);
+    backLetterMesh.position.x =  0.3;
+    backLetterMesh.position.y = 0.4;
     backLetterMesh.position.z = -this.cubeThick - this.cubeThick / 100;
     backLetterMesh.rotation.y = Math.PI;
 
@@ -432,7 +469,6 @@ export class AppComponent {
         this.INTERSECTED = hitPlate;
         // (this.INTERSECTED.material as THREE.MeshLambertMaterial).color.setHex(this.activeColor);
 
-
         let angleFrom = this.INTERSECTED.rotation.y;
 
         let angleDegFrom = angleFrom * 180 / Math.PI;
@@ -489,6 +525,21 @@ export class AppComponent {
     (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || (window as any).opera);
     return check;
   };
+
+  download = (filename: string, text: string): void => {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      pom.dispatchEvent(event);
+    }
+    else {
+      pom.click();
+    }
+  }
 
 }
 
