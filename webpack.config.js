8fd3e4fc7a618f9005b77ab69786322686e203c6
ulmer-morozov <ulmer.morozov@gmail.com>
2017-04-09 00:35:43 +0300

    Добавлены звуки

--- webpack.config.js
+++ webpack.config.js
@@ -6,81 +6,87 @@ const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
 
 var config = {
-    devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
-    context: path.resolve('./src'),
-    entry: {
-        app: './index.ts',
-        vendor: './vendor.ts'
-    },
-    output: {
-        path: path.resolve('./Explore'),
-        // publicPath: "/dist/",
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[name].map',
-        devtoolModuleFilenameTemplate: function(info) {
-            return "file:///" + info.absoluteResourcePath;
+  devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+  context: path.resolve('./src'),
+  entry: {
+    app: './index.ts',
+    vendor: './vendor.ts'
+  },
+  output: {
+    path: path.resolve('./Explore'),
+    // publicPath: path.resolve('./Explore'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    devtoolModuleFilenameTemplate: function(info) {
+      return "file:///" + info.absoluteResourcePath;
+    }
+  },
+  module: {
+    rules: [{
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: ["node_modules"],
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: "html"
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/,
+        loader: "file-loader?name=/assets/[name].[ext]",
+        query: {
+          useRelativePath: process.env.NODE_ENV === "production"
         }
-    },
-    module: {
-        rules: [{
-                enforce: 'pre',
-                test: /\.ts$/,
-                exclude: ["node_modules"],
-                loader: 'ts-loader'
-            },
-            {
-                test: /\.html$/,
-                loader: "html"
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css']
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            },
-
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-        modules: [path.resolve('./src'), 'node_modules']
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': { // eslint-disable-line quote-props
-                NODE_ENV: JSON.stringify(nodeEnv)
-            }
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Typescript Webpack Starter',
-            template: '!!ejs-loader!src/index.html'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: Infinity,
-            filename: 'vendor.bundle.js'
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            },
-            output: {
-                comments: false
-            },
-            sourceMap: false
-        }),
-        new DashboardPlugin(),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                tslint: {
-                    emitErrors: true,
-                    failOnHint: true
-                }
-            }
-        })
+      }
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: [path.resolve('./src'), 'node_modules']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { // eslint-disable-line quote-props
+        NODE_ENV: JSON.stringify(nodeEnv)
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Typescript Webpack Starter',
+      template: '!!ejs-loader!src/index.html'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }),
+    new DashboardPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        tslint: {
+          emitErrors: true,
+          failOnHint: true
+        }
+      }
+    })
+  ]
 };
 
 module.exports = config;
